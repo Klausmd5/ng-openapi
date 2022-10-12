3c3ff3d9995e88b58a7997c5075330e1c961da64
Klausmd5 <klaus.scheiboeck@vortexdata.net>
2022-10-12 14:35:38 +0200

    add appsettings.json

--- createNG.sh
+++ createNG.sh
@@ -4,6 +4,7 @@ loc=$(pwd)/$1
 ng new $1
 
 cd $loc
+npx npm-add-script -k swaggergen -v "openapi-generator-cli generate --generator-key angular"
 yarn add -D @openapitools/openapi-generator-cli
 
 cat $scr/app.module.ts > $loc/src/app/app.module.ts
